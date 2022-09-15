2315eace93eff12f3c04fd495a1110511819bacf
Peerawas Archavanuntakun <peerawas.archavanuntakun@dltx.io>
2022-09-15 13:48:52 +1000

    fix: baseUrl

--- src/providers/Api.tsx
+++ src/providers/Api.tsx
@@ -3,11 +3,7 @@ import Api from "../apis/Api";
 
 export const ApiContext = createContext<Api>(null as any);
 
-export const ApiProvider: React.FC<{ children: React.ReactNode }> = ({
-  children
-}) => {
-  // const api = new Api(process.env.REACT_APP_API_URL!);
-
+export const ApiProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const api = new Api();
   return <ApiContext.Provider value={api}>{children}</ApiContext.Provider>;
 };
