e500c7e904ee3d56d08cac97bffda5530c45e2a5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-10-29 22:05:35 +1000

    feat: remove erc20 code

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IMarket.sol";
 import "./IVault.sol";
 
-contract Vault is Ownable, IERC20, IVault {
+contract Vault is Ownable, ERC20, IVault {
     // ERC20
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
@@ -21,26 +21,6 @@ contract Vault is Ownable, IERC20, IVault {
     address private immutable _underlying;
     address private immutable _self;
     address private _market;
-
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public view returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
 
     function getMarket() external view returns (address) {
         return _market;
@@ -106,7 +86,7 @@ contract Vault is Ownable, IERC20, IVault {
         return IERC20(_underlying).balanceOf(_self);
     }
 
-    constructor(address underlying) {
+    constructor(address underlying) ERC20(_name, _symbol) {
         require(underlying != address(0), "Underlying address is invalid");
 
         _self = address(this);
@@ -115,46 +95,6 @@ contract Vault is Ownable, IERC20, IVault {
         _symbol = string(abi.encodePacked("HL", ERC20(underlying).symbol()));
         _name = string(abi.encodePacked("HL ", ERC20(underlying).name()));
         _decimals = ERC20(underlying).decimals();
-    }
-
-    function transfer(address to, uint256 amount) public returns (bool) {
-        address owner = _msgSender();
-        _transfer(owner, to, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        address owner = _msgSender();
-        _approve(owner, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        address spender = _msgSender();
-        _spendAllowance(from, spender, amount);
-        _transfer(from, to, amount);
-        return true;
-    }
-
-    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
-        address owner = _msgSender();
-        _approve(owner, spender, _allowances[owner][spender] + addedValue);
-        return true;
-    }
-
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
-        address owner = _msgSender();
-        uint256 currentAllowance = _allowances[owner][spender];
-        require(currentAllowance >= subtractedValue, "decreaseAllowance: Decreased allowance below zero");
-        unchecked {
-            _approve(owner, spender, currentAllowance - subtractedValue);
-        }
-
-        return true;
     }
 
     // Add underlying tokens to the pool
@@ -202,58 +142,6 @@ contract Vault is Ownable, IERC20, IVault {
         IERC20(_underlying).transfer(msg.sender, amount);
 
         emit Withdraw(msg.sender, balance);
-    }
-
-    function _mint(address account, uint256 amount) private {
-        require(account != address(0), "_mint: Mint to the zero address");
-
-        _totalSupply += amount;
-        _balances[account] += amount;
-        emit Transfer(address(0), account, amount);
-    }
-
-    function _burn(address account, uint256 amount) private {
-        require(account != address(0), "_burn: Burn from the zero address");
-
-        uint256 accountBalance = _balances[account];
-        require(accountBalance >= amount, "_burn: Burn amount exceeds balance");
-
-        _balances[account] -= amount;
-        _totalSupply -= amount;
-
-        emit Transfer(account, address(0), amount);
-    }
-
-    function _transfer(address from, address to,uint256 amount) internal {
-        require(from != address(0), "ERC20: Transfer from the zero address");
-        require(to != address(0), "ERC20: Transfer to the zero address");
-
-        uint256 fromBalance = _balances[from];
-        require(fromBalance >= amount, "ERC20: Transfer amount exceeds balance");
-        unchecked {
-            _balances[from] = fromBalance - amount;
-        }
-        _balances[to] += amount;
-
-        emit Transfer(from, to, amount);
-    }
-
-    function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "_approve: Approve from the zero address");
-        require(spender != address(0), "_approve: Approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    function _spendAllowance(address owner, address spender, uint256 amount) internal {
-        uint256 currentAllowance = allowance(owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            require(currentAllowance >= amount, "_spendAllowance: Insufficient allowance");
-            unchecked {
-                _approve(owner, spender, currentAllowance - amount);
-            }
-        }
     }
 
     modifier onlyMarket() {
