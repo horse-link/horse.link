ea6a673438e9cffb346f35e093b97f04595452a7
JasminFarhi <jasmin.farhi@dltx.io>
2022-10-31 14:41:15 +1000

    Added truncate for hash and link to Ethscan

--- src/pages/Faucet/FaucetModal.tsx
+++ src/pages/Faucet/FaucetModal.tsx
@@ -1,22 +1,32 @@
 import Modal from "../../components/Modal";
-
+import { FaRegShareSquare } from "react-icons/fa";
+import { truncateAddress } from "../../utils/shortenHash";
 type Props = {
   isOpen: boolean;
   onClose: () => void;
-  txHash?: string;
+  txHash?: string | undefined;
 };
 
 export const FaucetModal = ({ isOpen, onClose, txHash }: Props) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="w-152">
+      <div className="w-200">
         <h2>
           Tokens have been transferred.
           <br />
           It may take a few minutes to show up in your wallet.
         </h2>
         <br />
-        <p>Tx ID: {txHash}</p>
+        <div className="flex flex-row">
+          Tx ID: {truncateAddress(txHash)}
+          <a
+            href={`https://goerli.etherscan.io/tx/${txHash}`}
+            target="_blank"
+            className="pl-3 py-1"
+          >
+            <FaRegShareSquare />
+          </a>
+        </div>
       </div>
     </Modal>
   );
