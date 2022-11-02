781e835f3e2d4938f48ee942b84357c68353d8d7
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-11-02 20:40:36 +1000

    chore: move generic to swr not fetcher

--- src/hooks/useSwr.ts
+++ src/hooks/useSwr.ts
@@ -2,7 +2,7 @@ import swr from "swr";
 import fetcher from "../utils/fetcher";
 
 const useSwr = <T>(url: string) => {
-  const { data, error } = swr(url, fetcher<T>);
+  const { data, error } = swr<T>(url, fetcher);
 
   return {
     data,
