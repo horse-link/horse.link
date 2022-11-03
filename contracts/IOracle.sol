d22f68b40d682c57bbe539c4cc7353a2bd9bc52d
OptimusOpus <taylor.scheel@dltx.io>
2022-11-04 08:45:21 +1000

    Remove old contracts from repo

--- contracts/IOracle.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity =0.8.10;
-
-// Binary Oracle
-interface IOracle {
-    function getBinaryResult(bytes32 id) external returns (bool);
-}
--- contracts/IRewards.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity =0.8.10;
-
-interface IRewards {
-    function disburse(address to, uint256 amount) external;
-}
