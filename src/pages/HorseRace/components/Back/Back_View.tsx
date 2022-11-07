685ce4596fdcab8bfa500f6edb2fad795cb57296
pondpiu <peerawas.a@gmail.com>
2022-11-07 14:23:58 +1000

    chore: remove unnecessary form

--- src/pages/HorseRace/components/Back/Back_View.tsx
+++ src/pages/HorseRace/components/Back/Back_View.tsx
@@ -43,80 +43,73 @@ const BackView: React.FC<Props> = ({
     <div className="w-96 md:w-152">
       <div className="px-10 pt-5 pb-5 rounded-md bg-white border-gray-200 sm:rounded-lg">
         <div className="text-3xl">Target odds {back.odds}</div>
-        <form>
-          <div className="flex flex-col">
-            <label>Market</label>
-            <select
-              value={selectedMarket}
-              onChange={e => setSelectedMarket(e.target.value)}
-              name="markets"
-              id="markets"
-              className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            >
-              {markets.map(address => (
-                <MarketOption key={address} contractAddress={address} />
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col">
-            <label>
-              <span>Wager amount</span>
-              <input
-                type="number"
-                onChange={e => {
-                  updateWagerAmount(e.target.valueAsNumber);
-                }}
-                value={wagerAmount || ""}
-                placeholder="0.0"
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              />
-            </label>
 
-            <span className="p-1">
-              Potential Payout:
-              <input
-                type="number"
-                value={potentialPayout}
-                readOnly
-                placeholder="0.0"
-                className="rounded-md border-white focus:border-white focus:ring focus:ring-white focus:ring-opacity-50"
-              />
-            </span>
-          </div>
-          <br></br>
-          <div className="flex flex-col">
-            <RequireWalletButton
-              actionButton={
-                isEnoughAllowance ? (
-                  <button
-                    className="px-5 py-1 hover:bg-gray-100 rounded-md border border-gray-500 shadow-md"
-                    onClick={e => {
-                      e.preventDefault();
-                      contract.backContractWrite();
-                    }}
-                    disabled={shouldButtonDisabled}
-                  >
-                    {txStatus.isLoading ? <Loader /> : "Bet"}
-                  </button>
-                ) : (
-                  <button
-                    className={
-                      "px-5 py-1 hover:bg-gray-100 rounded-md border border-gray-500 shadow-md" +
-                      (!selectedMarket ? " opacity-50 cursor-not-allowed" : "")
-                    }
-                    onClick={e => {
-                      e.preventDefault();
-                      contract.approveContractWrite();
-                    }}
-                    disabled={!selectedMarket}
-                  >
-                    {txStatus.isLoading ? <Loader /> : "Approve"}
-                  </button>
-                )
-              }
+        <div className="flex flex-col">
+          <label>Market</label>
+          <select
+            value={selectedMarket}
+            onChange={e => setSelectedMarket(e.target.value)}
+            name="markets"
+            id="markets"
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          >
+            {markets.map(address => (
+              <MarketOption key={address} contractAddress={address} />
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-col">
+          <label>
+            <span>Wager amount</span>
+            <input
+              type="number"
+              onChange={e => {
+                updateWagerAmount(e.target.valueAsNumber);
+              }}
+              value={wagerAmount || ""}
+              placeholder="0.0"
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             />
-          </div>
-        </form>
+          </label>
+
+          <span className="p-1">
+            Potential Payout:
+            <input
+              type="number"
+              value={potentialPayout}
+              readOnly
+              placeholder="0.0"
+              className="rounded-md border-white focus:border-white focus:ring focus:ring-white focus:ring-opacity-50"
+            />
+          </span>
+        </div>
+        <br></br>
+        <div className="flex flex-col">
+          <RequireWalletButton
+            actionButton={
+              isEnoughAllowance ? (
+                <button
+                  className="px-5 py-1 hover:bg-gray-100 rounded-md border border-gray-500 shadow-md"
+                  onClick={() => contract.backContractWrite()}
+                  disabled={shouldButtonDisabled}
+                >
+                  {txStatus.isLoading ? <Loader /> : "Bet"}
+                </button>
+              ) : (
+                <button
+                  className={
+                    "px-5 py-1 hover:bg-gray-100 rounded-md border border-gray-500 shadow-md" +
+                    (!selectedMarket ? " opacity-50 cursor-not-allowed" : "")
+                  }
+                  onClick={() => contract.approveContractWrite()}
+                  disabled={!selectedMarket}
+                >
+                  {txStatus.isLoading ? <Loader /> : "Approve"}
+                </button>
+              )
+            }
+          />
+        </div>
       </div>
       <div className="mt-5">
         <ContractWriteResultCard
--- src/pages/VaultList/components/Vault/Vault_View.tsx
+++ src/pages/VaultList/components/Vault/Vault_View.tsx
@@ -45,70 +45,60 @@ const VaultView = ({
           <div className=" mx-auto p-5 mt-5 rounded-md border-white">
             <h1 className="text-3xl mb-2">Deposit / Withdraw</h1>
             <span>Shares: {userBalance ?? <Skeleton />}</span>
-            <form>
-              <label>
-                <span>Amount</span>
-                <input
-                  type="number"
-                  onChange={e => {
-                    updateAmount(e.target.valueAsNumber || 0);
-                  }}
-                  value={depositAmount || ""}
-                  placeholder="0.0"
-                  className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
-              </label>
-              <div className="mt-5">
-                <RequireWalletButton
-                  actionButton={
-                    isEnoughAllowance ? (
-                      <div className="flex flex-col">
-                        <button
-                          className={
-                            "px-5 py-1 hover:bg-gray-100 rounded-md border border-violet-700 shadow-md" +
-                            (shouldDepositButtonDisabled
-                              ? " opacity-50 cursor-not-allowed"
-                              : "")
-                          }
-                          onClick={e => {
-                            e.preventDefault();
-                            contract.depositContractWrite();
-                          }}
-                          disabled={shouldDepositButtonDisabled}
-                        >
-                          {txStatus.isLoading ? <Loader /> : "Deposit"}
-                        </button>
-                        <button
-                          className={
-                            "px-5 py-1 mt-2 hover:bg-gray-100 rounded-md border border-violet-700 shadow-md" +
-                            (shouldWithdrawButtonDisabled
-                              ? " opacity-50 cursor-not-allowed"
-                              : "")
-                          }
-                          onClick={e => {
-                            e.preventDefault();
-                            contract.withdrawContractWrite();
-                          }}
-                          disabled={shouldWithdrawButtonDisabled}
-                        >
-                          {txStatus.isLoading ? <Loader /> : "Withdraw"}
-                        </button>
-                      </div>
-                    ) : (
+
+            <label>
+              <span>Amount</span>
+              <input
+                type="number"
+                onChange={e => {
+                  updateAmount(e.target.valueAsNumber || 0);
+                }}
+                value={depositAmount || ""}
+                placeholder="0.0"
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </label>
+            <div className="mt-5">
+              <RequireWalletButton
+                actionButton={
+                  isEnoughAllowance ? (
+                    <div className="flex flex-col">
                       <button
-                        className="w-full px-5 py-1 hover:bg-gray-100 rounded-md border border-gray-500 shadow-md "
-                        onClick={e => {
-                          e.preventDefault();
-                          contract.approveContractWrite();
-                        }}
+                        className={
+                          "px-5 py-1 hover:bg-gray-100 rounded-md border border-violet-700 shadow-md" +
+                          (shouldDepositButtonDisabled
+                            ? " opacity-50 cursor-not-allowed"
+                            : "")
+                        }
+                        onClick={() => contract.depositContractWrite()}
+                        disabled={shouldDepositButtonDisabled}
                       >
-                        {txStatus.isLoading ? <Loader /> : "Approve"}
+                        {txStatus.isLoading ? <Loader /> : "Deposit"}
                       </button>
-                    )
-                  }
-                />
-              </div>
-            </form>
+                      <button
+                        className={
+                          "px-5 py-1 mt-2 hover:bg-gray-100 rounded-md border border-violet-700 shadow-md" +
+                          (shouldWithdrawButtonDisabled
+                            ? " opacity-50 cursor-not-allowed"
+                            : "")
+                        }
+                        onClick={() => contract.withdrawContractWrite()}
+                        disabled={shouldWithdrawButtonDisabled}
+                      >
+                        {txStatus.isLoading ? <Loader /> : "Withdraw"}
+                      </button>
+                    </div>
+                  ) : (
+                    <button
+                      className="w-full px-5 py-1 hover:bg-gray-100 rounded-md border border-gray-500 shadow-md "
+                      onClick={() => contract.approveContractWrite()}
+                    >
+                      {txStatus.isLoading ? <Loader /> : "Approve"}
+                    </button>
+                  )
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
