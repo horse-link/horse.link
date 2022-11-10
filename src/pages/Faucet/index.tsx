e72f9f4f0e8a15009395725c3855b1fc805de8be
JasminFarhi <jasmin.farhi@dltx.io>
2022-11-10 15:22:55 +1000

    Adjust text copy

--- src/pages/Faucet/index.tsx
+++ src/pages/Faucet/index.tsx
@@ -61,9 +61,9 @@ export const FaucetPage = () => {
           Please make sure you are connected to Goerli network.
         </h2>
         <p className="p-1">
-          To connect to Goerli, within the Metamask extension/dropdown, click
-          the Network tab at the top and select Goerli test network (this will
-          be there by default).
+          To connect to the Goerli network, select your Metamask extension and
+          click the Network tab at the top. Select the Goerli test network (this
+          will be there by default).
         </p>
       </div>
       <div className="flex gap-3 flex-wrap">
