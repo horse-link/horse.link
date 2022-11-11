b2e06953597af6a05b7957a601972b0e04916a0d
Peerawas Archavanuntakun <peerawas.archavanuntakun@dltx.io>
2022-11-11 16:01:43 +1000

    update text

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,6 +1,6 @@
 export const formatToFourDecimals = (amount: string) => {
   const parsedAmount = parseFloat(amount);
-  if (parsedAmount < 0.0001) return "0.0001<";
+  if (parsedAmount < 0.0001) return "<0.0001";
 
   const roundedToFourDecimal = parsedAmount.toFixed(4);
   const removedTrailingZeros = (+roundedToFourDecimal).toString();
