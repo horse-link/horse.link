714296f5f476003c70d40ff73fe78e1c401f7cc6
JasminFarhi <jasmin.farhi@dltx.io>
2022-11-15 09:14:58 +1000

    Removed styling for sm and lg view

--- src/pages/Dashboard/Dashboard_View.tsx
+++ src/pages/Dashboard/Dashboard_View.tsx
@@ -120,7 +120,7 @@ const Table: React.FC<TableProps> = (props: TableProps) => {
         </h3>
         <div className="flex flex-col">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="py-2 align-middle inline-block min-w-full">
               <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-200 bg-white">
                   <thead className="bg-gray-50">
