31b3c68940f0f414d973e5e6e9033bdaf052e273
Peerawas Archavanuntakun <peerawas.archavanuntakun@dltx.io>
2022-11-14 10:20:18 +1000

    feat: setup ci

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   jest: {
     configure: {
-      roots: ["<rootDir>/src", "<rootDir>/__tests__"],
-      testMatch: ["<rootDir>/__tests__/**/*.{spec,test}.{js,jsx,ts,tsx}"]
+      roots: ["<rootDir>/src"],
+      testMatch: ["<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}"]
     }
   },
   style: {
