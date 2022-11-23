8730baf772af40cdebc126c263b4b6748c5334fc
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-11-23 11:30:34 +1000

    chore: remove inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
