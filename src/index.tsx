0bbfbb43b41148778176dd5bebe8852699ee711a
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-11-23 11:23:15 +1000

    chore: lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import 'react-loading-skeleton/dist/skeleton.css'
+import "react-loading-skeleton/dist/skeleton.css";
 import App from "./App";
 
 const container = document.getElementById("root");
