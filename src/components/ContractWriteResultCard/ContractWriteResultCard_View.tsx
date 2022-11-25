3520b8dcbb29f9804bdfba0892795e6dd3161275
C00p3rb123 <48899363+C00p3rb123@users.noreply.github.com>
2022-11-25 14:26:17 +1000

    requested changes: link & correcting type usage

--- src/components/ContractWriteResultCard/ContractWriteResultCard_View.tsx
+++ src/components/ContractWriteResultCard/ContractWriteResultCard_View.tsx
@@ -18,13 +18,14 @@ const ContractWriteResultCard = ({
       {isSuccess && hash && (
         <div className="py-5 rounded-md shadow  bg-emerald-300 text-emerald-800 w-full text-center">
           <p className="p-1">Success! Your {txType} has been confirmed.</p>
-
+          Transaction Hash: &nbsp;
           <a
+            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
             href={`${process.env.VITE_SCANNER_URL}/tx/${hash}`}
             target="_blank"
             rel="noopener noreferrer"
           >
-            Transaction Hash: {shortenAddress(hash)}
+            {shortenAddress(hash)}
           </a>
         </div>
       )}
