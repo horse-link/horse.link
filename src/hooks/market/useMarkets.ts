f4282a123040a4b0cfb400ad0cf1cdc72b4e6c50
OptimusOpus <taylor.scheel@dltx.io>
2022-11-28 11:25:15 +1000

    Stop calling contract from FE

--- src/hooks/market/useMarkets.ts
+++ src/hooks/market/useMarkets.ts
@@ -1,44 +1,5 @@
-import { useContractRead, useContractReads } from "wagmi";
-import registryContractJson from "../../abi/Registry.json";
-import { BigNumber, ethers } from "ethers";
 import { useEffect, useState } from "react";
-import { useConfig } from "../../providers/Config";
 import api from "../../apis/Api";
-
-const useMarketAddressesFromContract = () => {
-  const config = useConfig();
-  const registryContractAddress = config?.addresses?.registry;
-  if (!registryContractAddress)
-    throw new Error("No REGISTRY_CONTRACT provided");
-
-  const registryContract = {
-    address: registryContractAddress,
-    abi: registryContractJson.abi
-  };
-
-  const { data } = useContractRead({
-    ...registryContract,
-    functionName: "marketCount"
-  });
-  const marketCountData = data as BigNumber;
-  const marketCountStr =
-    marketCountData && ethers.utils.formatUnits(marketCountData, 0);
-  const marketCount = parseInt(marketCountStr ?? "0");
-
-  const { data: marketsData } = useContractReads({
-    contracts: Array.from({ length: marketCount }, (_, i) => {
-      return {
-        ...registryContract,
-        functionName: "markets",
-        args: [i]
-      };
-    })
-  });
-
-  const marketAddresses = marketsData?.filter(v => v) ?? [];
-
-  return { marketAddresses: marketAddresses as unknown as string[] };
-};
 
 const useMarketAddressesFromAPI = () => {
   const [marketAddresses, setMarketAddresses] = useState<string[]>([]);
@@ -53,14 +14,8 @@ const useMarketAddressesFromAPI = () => {
   return { marketAddresses: marketAddresses as unknown as string[] };
 };
 
-const shouldUseAPI = process.env.VITE_REST_FOR_MARKETS;
-
 const useMarkets = () => {
-  if (shouldUseAPI) {
-    const { marketAddresses } = useMarketAddressesFromAPI();
-    return { marketAddresses };
-  }
-  const { marketAddresses } = useMarketAddressesFromContract();
+  const { marketAddresses } = useMarketAddressesFromAPI();
   return { marketAddresses };
 };
 
--- src/hooks/vault/useVaults.ts
+++ src/hooks/vault/useVaults.ts
@@ -1,44 +1,5 @@
-import { useContractRead, useContractReads } from "wagmi";
-import registryContractJson from "../../abi/Registry.json";
-import { BigNumber, ethers } from "ethers";
 import { useEffect, useState } from "react";
 import api from "../../apis/Api";
-import { useConfig } from "../../providers/Config";
-
-const useVaultAddresesFromContract = () => {
-  const config = useConfig();
-  const registryContractAddress = config?.addresses?.registry;
-  if (!registryContractAddress)
-    throw new Error("No REGISTRY_CONTRACT provided");
-
-  const registryContract = {
-    address: registryContractAddress,
-    abi: registryContractJson.abi
-  };
-
-  const { data: vaultCountData } = useContractRead({
-    ...registryContract,
-    functionName: "vaultCount"
-  });
-
-  const vaultCountStr =
-    vaultCountData && ethers.utils.formatUnits(vaultCountData as BigNumber, 0);
-  const vaultCount = parseInt((vaultCountStr as string) ?? "0");
-
-  const { data: vaultsData } = useContractReads({
-    contracts: Array.from({ length: vaultCount }, (_, i) => {
-      return {
-        ...registryContract,
-        functionName: "vaults",
-        args: [i]
-      };
-    })
-  });
-
-  const vaultAddresses = vaultsData?.filter(v => v) ?? [];
-
-  return { vaultAddresses: vaultAddresses as unknown as string[] };
-};
 
 const useVaultAddresesFromAPI = () => {
   const [vaultAddresses, setVaultAddresses] = useState<string[]>([]);
@@ -53,14 +14,8 @@ const useVaultAddresesFromAPI = () => {
   return { vaultAddresses: vaultAddresses as unknown as string[] };
 };
 
-const shouldUseAPI = process.env.VITE_REST_FOR_VAULTS;
-
 const useVaults = () => {
-  if (shouldUseAPI) {
-    const { vaultAddresses } = useVaultAddresesFromAPI();
-    return { vaultAddresses };
-  }
-  const { vaultAddresses } = useVaultAddresesFromContract();
+  const { vaultAddresses } = useVaultAddresesFromAPI();
   return { vaultAddresses };
 };
 
