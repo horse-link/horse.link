4f63dcc05015f42d677fef5af0d15c53e78c79ac
JasminFarhi <jasmin.farhi@dltx.io>
2022-12-08 16:27:25 +1000

    Add styling to boxes

--- src/components/Dashboard/NextToJumpBanner.tsx
+++ src/components/Dashboard/NextToJumpBanner.tsx
@@ -13,30 +13,22 @@ export const NextToJumpBanner: React.FC<Props> = ({ NextToJump }) => {
     // console.log(NextToJump., "COOPER");
   }
   return (
-    <div className="bg-indigo-500  mt-5 rounded-lg mb-5">
-      <div className="max-w-screen-2xl mx-auto sm:px-6 lg:px-8">
+    <div className="bg-indigo-500  mt-5 rounded-lg mb-5 overflow-x-scroll">
+      <div className="mx-auto sm:px-6 lg:px-8">
         <div className="flex justify-between p-5">
-          <div className="flex">
-            <div className="overflow-x-scroll sm:-my-px sm:flex sm:space-x-2">
-              <div className="flex flex-row divide-x-4 pl-2 divide-indigo-700 mx-2 ml-4">
-                {NextToJump?.map(jumper => (
-                  <div className="text-xs">
-                    {`${jumper.meeting.jumperMeetingName} (${
-                      jumper.meeting.location
-                    }) - R${jumper.jumperRaceNumber}
-                  ${moment(jumper.jumperRaceStartTime).fromNow(true)}`}
-                  </div>
-                ))}
+          <div className="flex flex-row divide-x-2 pl-2 divide-indigo-700">
+            {NextToJump?.map(jumper => (
+              <div className="text-xs px-5 font-medium ">
+                {`${jumper.meeting.jumperMeetingName} (${jumper.meeting.location}) - R${jumper.jumperRaceNumber}
+                `}
+                <div className="box-border text-sx font-bold">
+                  {` ${moment(jumper.jumperRaceStartTime).fromNow(true)}`}
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-// ${moment
-//   .utc(jumper.jumperRaceStartTime)
-//   .local()
-//   .format("H:mm")}
