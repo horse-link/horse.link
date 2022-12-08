1c91b53a513115278734cb55a7e35208a40f8b37
OptimusOpus <taylor.scheel@dltx.io>
2022-12-08 16:29:58 +1000

    Add bet history to race page

--- src/pages/Races.tsx
+++ src/pages/Races.tsx
@@ -5,10 +5,13 @@ import { useAccount } from "wagmi";
 import { useWalletModal } from "../providers/WalletModal";
 import moment from "moment";
 import { RaceTable } from "../components/Races";
-import { PlaceBetModal } from "../components/Modals";
+import { PlaceBetModal, SettleBetModal } from "../components/Modals";
 import utils from "../utils";
 import { Runner } from "../types/meets";
 import { PageLayout } from "../components";
+import { useSubgraphBets } from "../hooks/subgraph";
+import { BetHistory } from "../types/bets";
+import { BetTable } from "../components/Bets";
 
 export const Races: React.FC = () => {
   const params = useParams();
@@ -16,9 +19,12 @@ export const Races: React.FC = () => {
   const raceNumber = Number(params.number) || 0;
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSettleModalOpen, setSettleIsModalOpen] = useState(false);
   const [selectedRunner, setSelectedRunner] = useState<Runner>();
+  const [selectedBet, setSelectedBet] = useState<BetHistory>();
 
   const { runners } = useRunnersData(track, raceNumber);
+  const { betHistory, refetch } = useSubgraphBets(false, "ALL_BETS");
 
   const { isConnected } = useAccount();
   const { openWalletModal } = useWalletModal();
@@ -33,6 +39,13 @@ export const Races: React.FC = () => {
     if (!runner) return;
     setSelectedRunner(runner);
     openDialog();
+  };
+
+  const onClickBet = (betData?: BetHistory) => {
+    if (!betData) return;
+    if (!isConnected) return openWalletModal();
+    setSelectedBet(betData);
+    setSettleIsModalOpen(true);
   };
 
   return (
@@ -51,6 +64,20 @@ export const Races: React.FC = () => {
         <RaceTable
           runners={runners || utils.mocks.getMockRunners()}
           onClickRunner={onClickRunner}
+        />
+      </div>
+      <div className="flex flex-col gap-6">
+        <h1 className="text-2xl font-bold mt-4">Race Bet History</h1>
+        <BetTable
+          myBetsEnabled={false}
+          onClickBet={onClickBet}
+          betHistory={betHistory}
+        />
+        <SettleBetModal
+          isModalOpen={isSettleModalOpen}
+          setIsModalOpen={setSettleIsModalOpen}
+          selectedBet={selectedBet}
+          refetch={refetch}
         />
       </div>
     </PageLayout>
