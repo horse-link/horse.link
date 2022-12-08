6d6b18c32b150042ab5edaa3b129c57c18af3863
JasminFarhi <jasmin.farhi@dltx.io>
2022-12-08 16:43:06 +1000

    Adjust styling colours

--- src/components/Dashboard/NextToJumpBanner.tsx
+++ src/components/Dashboard/NextToJumpBanner.tsx
@@ -6,22 +6,16 @@ type Props = {
 };
 
 export const NextToJumpBanner: React.FC<Props> = ({ NextToJump }) => {
-  {
-    NextToJump?.filter(jumper =>
-      console.log(jumper.jumperRaceStartTime, "JASMIN")
-    );
-    // console.log(NextToJump., "COOPER");
-  }
   return (
-    <div className="bg-indigo-500  mt-5 rounded-lg mb-5 overflow-x-scroll">
+    <div className="bg-white mt-5 rounded-lg mb-5 overflow-x-scroll">
       <div className="mx-auto sm:px-6 lg:px-8">
         <div className="flex justify-between p-5">
-          <div className="flex flex-row divide-x-2 pl-2 divide-indigo-700">
+          <div className="flex flex-row divide-x-2 pl-2 divide-emerald-600">
             {NextToJump?.map(jumper => (
-              <div className="text-xs px-5 font-medium ">
+              <div className="text-xs px-3 font-medium font-white text-emerald-800">
                 {`${jumper.meeting.jumperMeetingName} (${jumper.meeting.location}) - R${jumper.jumperRaceNumber}
                 `}
-                <div className="box-border text-sx font-bold">
+                <div className="box-border text-sx font-bold text-emerald-300">
                   {` ${moment(jumper.jumperRaceStartTime).fromNow(true)}`}
                 </div>
               </div>
