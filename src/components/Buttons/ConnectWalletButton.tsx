bf4b7d60cbcf01b6e6af168ae885a8e7e7b05acb
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-12-12 13:06:32 +1000

    chore: shorten address

--- src/components/Buttons/ConnectWalletButton.tsx
+++ src/components/Buttons/ConnectWalletButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAccount, useDisconnect } from "wagmi";
 import { BaseButton } from ".";
+import utils from "../../utils";
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   openWalletModal: () => void;
@@ -11,34 +12,32 @@ export const ConnectWalletButton: React.FC<Props> = ({ openWalletModal }) => {
   const { disconnect } = useDisconnect();
 
   return (
-    <>
-      <div
-        className={
-          isConnecting
-            ? "hidden"
-            : "inline-flex items-center text-sm font-medium justify-self-end"
-        }
-      >
-        {isConnected ? (
-          <div className="flex items-center gap-3">
-            <span className="truncate text-gray-500">{address}</span>
-            <div>
-              <BaseButton
-                className="cursor-pointer hover:bg-gray-200 hover:text-white sm:w-auto sm:mb-0"
-                onClick={() => disconnect()}
-              >
-                Disconnect
-              </BaseButton>
-            </div>
-          </div>
-        ) : (
-          <div className="mx-3">
-            <BaseButton onClick={openWalletModal}>
-              Connect your Wallet
+    <div
+      className={
+        isConnecting
+          ? "hidden"
+          : "w-full flex justify-center items-center text-sm font-medium justify-self-end"
+      }
+    >
+      {isConnected && address ? (
+        <div className="flex items-center gap-3">
+          <span className="text-gray-500">
+            {utils.formatting.shortenAddress(address)}
+          </span>
+          <div>
+            <BaseButton
+              className="cursor-pointer hover:bg-gray-200 hover:text-white sm:w-auto sm:mb-0"
+              onClick={() => disconnect()}
+            >
+              Disconnect
             </BaseButton>
           </div>
-        )}
-      </div>
-    </>
+        </div>
+      ) : (
+        <div className="mx-3">
+          <BaseButton onClick={openWalletModal}>Connect your Wallet</BaseButton>
+        </div>
+      )}
+    </div>
   );
 };
