067735f7a196da473e6614f924411cc598f465ca
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-12-13 16:15:08 +1000

    fix: swr

--- src/hooks/useSwr.ts
+++ src/hooks/useSwr.ts
@@ -5,7 +5,14 @@ const client = utils.general.getAxiosClient();
 const fetcher = utils.general.getAxiosFetcher(client);
 
 const useSwr = <T>(url: string) => {
-  const { data, error } = swr<T>(url, fetcher);
+  const { data, error } = swr<T>(url, fetcher, {
+    // refresh data every second
+    refreshInterval: 1000,
+    // even when tab is closed
+    refreshWhenHidden: true,
+    // also attempt if connection drops
+    refreshWhenOffline: true
+  });
 
   return {
     data,
