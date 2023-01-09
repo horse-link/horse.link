bb944635cea9bc92280baa451067bcca642bb681
C00p3rb123 <48899363+C00p3rb123@users.noreply.github.com>
2023-01-09 14:50:38 +1000

    HL-358

--- src/components/Dashboard/DashboardBannerRow.tsx
+++ src/components/Dashboard/DashboardBannerRow.tsx
@@ -2,16 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { NextToJump } from "../../types/meets";
 import utils from "../../utils";
+import { Config } from "../../types/config";
+import { getVenuefromConfig } from "../../utils/config";
 
 const INTERVAL = 1000;
 
 type Props = {
   meet: NextToJump;
+  config?: Config;
 };
 
-export const DashboardBannerRow: React.FC<Props> = ({ meet }) => {
+export const DashboardBannerRow: React.FC<Props> = ({ meet, config }) => {
   const [timeString, setTimeString] = useState(
     utils.formatting.formatTimeToHMS(meet.jumperRaceStartTime)
+  );
+  const venueLocation = getVenuefromConfig(
+    meet.meeting.jumperMeetingName,
+    config
   );
 
   useEffect(() => {
@@ -22,10 +29,10 @@ export const DashboardBannerRow: React.FC<Props> = ({ meet }) => {
     return () => clearInterval(interval);
   }, [meet]);
 
-  return (
+  return venueLocation ? (
     <Link
-      className="w-full shrink-0 h-full lg:shrink flex flex-col items-center"
-      to={`/horses/${meet.meeting.meetID}/${meet.jumperRaceNumber}`}
+      className="w-full shrink-0 h-full lg:shrink flex flex-col text-center hover:bg-indigo-900 p-2"
+      to={`/horses/${venueLocation}/${meet.jumperRaceNumber}`}
     >
       <div>
         <span className="block">
@@ -34,5 +41,12 @@ export const DashboardBannerRow: React.FC<Props> = ({ meet }) => {
         <span className="block font-semibold">{` ${timeString}`}</span>
       </div>
     </Link>
+  ) : (
+    <div className="w-full shrink-0 h-full lg:shrink flex flex-col text-center hover:bg-green-700 p-2">
+      <span className="block">
+        {`${meet.meeting.jumperMeetingName} (${meet.meeting.location}) - R${meet.jumperRaceNumber}`}
+      </span>
+      <span className="block font-semibold">{` ${timeString}`}</span>
+    </div>
   );
 };
