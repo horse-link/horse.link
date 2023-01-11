5c1d01ebb25f7c064816e9908371786763ba9455
OptimusOpus <taylor.scheel@dltx.io>
2023-01-11 10:41:36 +1000

    Only shorten hashes on small screens

--- src/components/Modals/FaucetModal.tsx
+++ src/components/Modals/FaucetModal.tsx
@@ -10,13 +10,10 @@ type Props = {
 export const FaucetModal: React.FC<Props> = ({ isOpen, onClose, txHash }) => (
   <BaseModal isOpen={isOpen} onClose={onClose}>
     <div className="max-w-[95vw] lg:min-w-[28rem]">
-      <h2 className="font-bold text-2xl mr-[8vw] mb-2">Transaction result</h2>
-      <br />
       <Web3SuccessHandler
         hash={txHash}
         message="Your tokens have been placed with"
       />
-      <br />
       <h2>It may take a few minutes to show up in your wallet.</h2>
     </div>
   </BaseModal>
--- src/components/Web3Handlers/Web3SuccessHandler.tsx
+++ src/components/Web3Handlers/Web3SuccessHandler.tsx
@@ -9,8 +9,8 @@ type Props = {
 export const Web3SuccessHandler: React.FC<Props> = ({ hash, message }) => (
   <div className="py-4 rounded-md flex flex-col">
     <h2 className="font-bold text-2xl mr-[8vw] mb-2">Transaction result</h2>
-    <span className="block">
-      {`${message} `}
+    <span className="block lg:hidden">
+      {`${message}: `}
       <a
         className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
         href={`${process.env.VITE_SCANNER_URL}/tx/${hash}`}
@@ -18,6 +18,18 @@ export const Web3SuccessHandler: React.FC<Props> = ({ hash, message }) => (
         rel="noreferrer noopener"
       >
         {utils.formatting.shortenAddress(hash)}
+      </a>
+    </span>
+    <span className="hidden lg:block">
+      {`${message}: `}
+      <br />
+      <a
+        className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        href={`${process.env.VITE_SCANNER_URL}/tx/${hash}`}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        {hash}
       </a>
     </span>
   </div>
