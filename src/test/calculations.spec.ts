db299fe1e5b816604a7226dbe7d869f44acc3873
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-01-18 11:19:48 +1000

    chore: cleanup test

--- src/test/calculations.spec.ts
+++ src/test/calculations.spec.ts
@@ -3,17 +3,19 @@ import { expect } from "chai";
 
 describe("Calculation tests", () => {
   it("Should correctly calculate odds margin", () => {
+    // using nine units of precision as displayed by ticket
+    const PRECISION = 9;
+
     const oddsArray = [2.3];
     const margin = utils.races.calculateRaceMargin(oddsArray);
 
-    // using nine units of precision as displayed by ticket
-    expect((+margin).toFixed(9)).to.equal("0.434782609");
+    expect((+margin).toFixed(PRECISION)).to.equal("0.434782609");
 
     // same units as on ticket
     const biggerOddsArray = [2.3, 3.7, 6.5, 10, 6.5, 26, 15, 19, 81, 71];
     const biggerMargin = utils.races.calculateRaceMargin(biggerOddsArray);
 
-    expect((+biggerMargin).toFixed(9)).to.equal("1.296935157");
+    expect((+biggerMargin).toFixed(PRECISION)).to.equal("1.296935157");
 
     // display test
     const display = utils.formatting.formatToTwoDecimals(
