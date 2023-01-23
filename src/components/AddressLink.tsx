56da29ac04b6878ee907a70a211c24545ff822b5
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-01-24 09:14:26 +1000

    fix: comments

--- src/components/AddressLink.tsx
+++ src/components/AddressLink.tsx
@@ -1,13 +1,19 @@
 import { Address } from "wagmi";
 import constants from "../constants";
+import utils from "../utils";
 
-export const AddressLink: React.FC<{ address: Address }> = props => (
+type Props = {
+  address: Address;
+  isTruncated?: boolean;
+};
+
+export const AddressLink: React.FC<Props> = ({ address, isTruncated }) => (
   <a
-    href={`${constants.env.SCANNER_URL}/address/${props.address}`}
+    href={`${constants.env.SCANNER_URL}/address/${address}`}
     target="_blank"
     rel="noreferrer noopener"
-    className="text-blue-600"
+    className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
   >
-    {props.address}
+    {isTruncated ? utils.formatting.shortenAddress(address) : address}
   </a>
 );
