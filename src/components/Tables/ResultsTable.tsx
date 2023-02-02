7e4449f6eec2eeaeed994e60d85c4128dae236df
pondpiu <peerawas.a@gmail.com>
2023-02-02 13:04:35 +0700

    move format ordinal and add test

--- src/components/Tables/ResultsTable.tsx
+++ src/components/Tables/ResultsTable.tsx
@@ -2,23 +2,10 @@ import React from "react";
 import { MeetResults, WinningHorse } from "../../types/meets";
 import { BaseTable } from ".";
 import { TableData, TableHeader, TableRow } from "../../types/table";
+import { formatOrdinals } from "../../utils/formatting";
 
 type Props = {
   results: MeetResults;
-};
-
-const pr = new Intl.PluralRules("en-US", { type: "ordinal" });
-
-const suffixes = new Map([
-  ["one", "st"],
-  ["two", "nd"],
-  ["few", "rd"],
-  ["other", "th"]
-]);
-const formatOrdinals = (n: number) => {
-  const rule = pr.select(n);
-  const suffix = suffixes.get(rule);
-  return `${n}${suffix}`;
 };
 
 export const ResultsTable: React.FC<Props> = ({ results }) => {
