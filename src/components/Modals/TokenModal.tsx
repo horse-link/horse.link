25c81dd50f3c89f5920ed433677abb54df6b58af
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-02-08 15:42:30 +1000

    feat: address on token modal

--- src/components/Modals/TokenModal.tsx
+++ src/components/Modals/TokenModal.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Token } from "../../types/tokens";
 import { BaseModal } from "./BaseModal";
 import { Loader } from "../Loader";
 import { useTokenContext } from "../../providers/Token";
+import utils from "../../utils";
 
 type Props = {
   isOpen: boolean;
@@ -19,6 +20,14 @@ export const TokenModal: React.FC<Props> = ({
 }) => {
   const { changeToken } = useTokenContext();
 
+  const onClick = useCallback(
+    (t: Token) => {
+      changeToken(t);
+      onClose();
+    },
+    [changeToken, onClose]
+  );
+
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       {tokensLoading || !availableTokens || !availableTokens.length ? (
@@ -26,19 +35,19 @@ export const TokenModal: React.FC<Props> = ({
           <Loader />
         </div>
       ) : (
-        <div className="max-w-[95vw] lg:min-w-[28rem]">
+        <div className="w-[95vw] max-w-full lg:max-w-[28rem]">
           <h2 className="text-xl font-bold">Available Tokens</h2>
           <div className="mt-4 w-full">
             {availableTokens.map(t => (
               <div className="w-full">
                 <button
-                  className="w-full rounded-md py-1 hover:bg-zinc-100"
-                  onClick={() => {
-                    changeToken(t);
-                    onClose();
-                  }}
+                  className="flex w-full justify-between rounded-md py-1 px-2 hover:bg-zinc-100"
+                  onClick={() => onClick(t)}
                 >
-                  {t.symbol}
+                  <span className="block">{t.symbol}</span>
+                  <span className="block">
+                    {utils.formatting.shortenAddress(t.address)}
+                  </span>
                 </button>
               </div>
             ))}
