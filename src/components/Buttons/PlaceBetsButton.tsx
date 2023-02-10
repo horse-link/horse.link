47e5cf188d121c6c17a2153edee719bf1a80a5a5
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-02-10 15:26:17 +1000

    fix: button spinner

--- src/components/Buttons/PlaceBetsButton.tsx
+++ src/components/Buttons/PlaceBetsButton.tsx
@@ -8,10 +8,11 @@ export const PlaceBetsButton: React.FC = () => {
   return (
     <BaseButton
       loading={txLoading}
+      loaderColor="black"
       disabled={!bets || !bets.length || txLoading}
       onClick={placeBets}
       baseStyleOverride
-      className="w-full !font-bold text-lg border-2 border-black rounded-lg text-black text-center disabled:hover:bg-white hover:bg-black hover:text-white py-3"
+      className="w-full !font-bold text-lg border-2 border-black rounded-lg text-black text-center disabled:hover:bg-white hover:bg-black hover:text-white py-3 !opacity-100"
     >
       BET NOW
     </BaseButton>
