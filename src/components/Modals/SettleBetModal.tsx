d71aa6d3e0f1fc405f9842c6873ab32eb6a1834e
JasminFarhi <jasmin.farhi@dltx.io>
2023-02-13 16:03:08 +1000

    Adjust for review request

--- src/components/Modals/SettleBetModal.tsx
+++ src/components/Modals/SettleBetModal.tsx
@@ -187,7 +187,7 @@ export const SettleBetModal: React.FC<Props> = ({
                   {txLoading ? <Loader /> : "SETTLE BET"}
                 </button>
                 {!bet.marketResultAdded && (
-                  <span className="relative top-[0.5rem] bottom-[1rem] block text-xs text-black/80">
+                  <span className="relative mt-2 block text-xs text-black/80">
                     Note: will require two transactions to add market results
                     first
                   </span>
