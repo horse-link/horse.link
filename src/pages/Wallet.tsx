5e401a19f14fe4c44831eaebbaa30ec3db1ef8b6
pondpiu <peerawas.a@gmail.com>
2023-02-16 16:09:24 +1000

    remove unnecessary

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -7,7 +7,7 @@ const WalletPage = () => {
     <PageLayout>
       <div className="flex h-full flex-col justify-center">
         <h1 className="text-2xl font-bold">Wallet</h1>
-        <div className="mt-2 flex justify-center rounded-lg bg-white px-4 py-5 shadow sm:p-6 lg:mb-10">
+        <div className="mt-2 flex justify-center rounded-lg bg-white px-4 py-5 shadow">
           <div className="w-4/5 max-w-2xl">
             <div className="flex flex-col">
               <h2 className="text-lg font-bold">Mnemonic :</h2>
