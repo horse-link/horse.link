47585994a6b55da27039d63c73d6e144c9a94323
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-02-21 08:01:50 +1000

    fix: button width

--- src/components/Buttons/ClaimTokensButton.tsx
+++ src/components/Buttons/ClaimTokensButton.tsx
@@ -11,20 +11,15 @@ export const ClaimTokensButton: React.FC<Props> = ({ tokenName, onClick }) => {
 
   const click = async () => {
     setLoading(true);
-    try {
-      await onClick();
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setLoading(false);
-    }
+    await onClick().catch(console.error);
+    setLoading(false);
   };
 
   return (
     <BaseButton
       title={`Claim ${tokenName}`}
       onClick={click}
-      className="mr-4 rounded-md border-2 border-black px-4 py-2 font-bold text-black transition-colors duration-100 enabled:hover:bg-black enabled:hover:text-white"
+      className="mr-4 rounded-md border-2 border-black px-4 py-2 font-bold text-black transition-colors duration-100 enabled:hover:bg-black enabled:hover:text-white w-full lg:w-1/2"
       baseStyleOverride
       loading={loading}
     />
