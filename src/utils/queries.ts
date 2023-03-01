79b485b26df463d0e772c2667fedc4d8527a1684
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-01 15:47:48 +1000

    fix: subgraph filter

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -24,8 +24,7 @@ const getOptionalFilterOptions = (filter?: BetFilterOptions) => {
     case "ALL_BETS":
       return "";
     case "PENDING":
-      // TODO: filter here instead of after formatBetHistory when subgraph is updated with market oracle as data source
-      return "";
+      return `payoutAt_gt: ${Math.floor(Date.now() / 1000)}`;
     case "RESULTED":
       return `settled: false`;
     case "SETTLED":
