40552a78d89057929ef8cdefc0ccd4856be61558
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-14 14:31:06 +1000

    fix: fetcher

--- src/hooks/useSwr.ts
+++ src/hooks/useSwr.ts
@@ -1,11 +1,15 @@
 import swr from "swr";
 import utils from "../utils";
 import constants from "../constants";
-
-const client = utils.general.getAxiosClient();
-const fetcher = utils.general.getAxiosFetcher(client);
+import { useNetwork } from "wagmi";
 
 const useSwr = <T>(url: string, interval = constants.time.ONE_SECOND_MS) => {
+  const { chain: network } = useNetwork();
+  const isOurApi = url.startsWith("/");
+
+  const client = utils.general.getAxiosClient(isOurApi ? network : undefined);
+  const fetcher = utils.general.getAxiosFetcher(client);
+
   const { data, error } = swr<T>(url, fetcher, {
     // refresh data interval
     refreshInterval: interval,
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,15 +1,14 @@
 import axios, { AxiosInstance } from "axios";
 import constants from "../constants";
 import { Network } from "../types/general";
-import { chain } from "wagmi";
 
 // default network will be goerli
-export const getAxiosClient = (currentNetWork: Network = chain.goerli) =>
+export const getAxiosClient = (currentNetwork?: Network) =>
   axios.create({
     baseURL: constants.env.API_URL,
     headers: {
       Accept: "application/json",
-      "chain-id": currentNetWork.id
+      "chain-id": currentNetwork ? currentNetwork.id : undefined
     }
   });
 
