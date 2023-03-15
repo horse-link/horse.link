3b5112adeef3d78a98769a62485391ce8320753b
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-16 09:25:31 +1000

    feat: no stats handling

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -12,17 +12,24 @@ const Leaderboard: React.FC = () => {
   const { isConnected } = useAccount();
   const [showLeaderboard, setShowLeaderboard] = useState(false);
 
+  const userHasNoStats = isConnected && !userStats;
+
   return (
     <PageLayout>
       {showLeaderboard ? (
         <React.Fragment>
           {isConnected && (
             <div className="mb-4 flex w-full flex-col justify-center gap-x-1 gap-y-2 text-left md:flex-row lg:justify-between lg:gap-x-4">
-              <Card title="Your Rank" data={userStats?.rank.toString()} />
+              <Card
+                title="Your Rank"
+                data={userHasNoStats ? "N/A" : userStats?.rank.toString()}
+              />
               <Card
                 title="Your Earnings"
                 data={
-                  userStats?.earnings.value
+                  userHasNoStats
+                    ? "0"
+                    : userStats?.earnings.value
                     ? `${utils.formatting.formatToFourDecimalsRaw(
                         ethers.utils.formatEther(userStats.earnings.value)
                       )} HL`
@@ -32,7 +39,9 @@ const Leaderboard: React.FC = () => {
               <Card
                 title="Your Balance"
                 data={
-                  userStats?.balance.formatted
+                  userHasNoStats
+                    ? "0"
+                    : userStats?.balance.formatted
                     ? `${utils.formatting.formatToFourDecimals(
                         userStats.balance.formatted
                       )} HL`
