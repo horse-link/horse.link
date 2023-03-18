47cd6ed6ba01274e57732347d95892be09eef21c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-03-18 13:50:58 +1000

    chore:  add fx icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -11,12 +11,16 @@ export const SVG_ICONS: Record<string, SvgIcon> = {
 export { FaHorseHead as HorseHeadIcon } from "react-icons/fa";
 export { default as TokenomicsChart } from "./chart.svg";
 
+import { default as fxAUD } from "./fxAUD.png";
+import { default as fxUSD } from "./fxUSD.png";
 import { default as USDT } from "./tether.png";
 import { default as DAI } from "./dai.png";
 import { default as HL } from "./hl.png";
 import { SvgIcon } from "../types/general";
 
 export const CRYPTO_ICONS: Record<string, string> = {
+  fxAUD,
+  fxUSD,
   USDT,
   DAI,
   HL
