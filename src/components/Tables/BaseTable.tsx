086a3c3ffe29888063df6e4e498659731b475121
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-23 12:05:09 +1000

    chore: consolidate styles

--- src/components/Tables/BaseTable.tsx
+++ src/components/Tables/BaseTable.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { TableHeader, TableRow } from "../../types/table";
-import classnames from "classnames";
+import classNames from "classnames";
 
 type Props = {
   headers: TableHeader[];
   rows: TableRow[];
   title?: string;
   tableStyles?: string;
+  leftColumnSticky?: boolean;
 };
 
 export const BaseTable: React.FC<Props> = props => {
   const { headers, rows, tableStyles, title } = props;
 
   const headerStyles =
-    "px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase";
+    "px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase whitespace-nowrap";
   const cellStyles = "px-2 py-4 whitespace-nowrap";
 
   const createHeaderKey = (i: number) => `header-${i}`;
@@ -22,57 +23,64 @@ export const BaseTable: React.FC<Props> = props => {
 
   return (
     <div
-      className={classnames(
-        "flex flex-col overflow-x-scroll overflow-y-hidden lg:overflow-x-hidden",
-        {
-          [tableStyles || ""]: !!tableStyles
-        }
-      )}
+      className={classNames("flex w-full flex-col overflow-x-auto", {
+        [tableStyles!]: !!tableStyles
+      })}
     >
       {title && (
-        <h3 className="mb-3 text-lg font-medium text-gray-900">{title}</h3>
+        <h3 className="mb-3 w-full text-lg font-medium text-gray-900">
+          {title}
+        </h3>
       )}
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  {headers.map((header, i) => (
-                    <th
-                      key={createHeaderKey(i)}
-                      scope="col"
-                      className={`${headerStyles} ${header.classNames || ""}`}
-                    >
-                      {header.title}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 bg-white">
-                {rows.map(({ data, row }, i) => (
-                  <tr
-                    key={createRowKey(i)}
-                    className={row?.classNames || ""}
-                    role="row"
-                    {...row?.props}
+      <div className="w-full overflow-x-scroll">
+        <table className="w-full divide-y divide-gray-200 overflow-x-auto">
+          <thead className="bg-gray-50">
+            <tr>
+              {headers.map((header, i, a) => (
+                <th
+                  key={createHeaderKey(i)}
+                  scope="col"
+                  className={classNames(headerStyles, {
+                    [header.classNames!]: !!header.classNames,
+                    "rounded-tl-lg": i === 0,
+                    "rounded-tr-lg": i === a.length - 1
+                  })}
+                >
+                  {header.title}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200 bg-white">
+            {rows.map(({ data, row }, rowIndex, rowArray) => (
+              <tr
+                key={createRowKey(rowIndex)}
+                className={classNames({
+                  [row?.classNames!]: !!row?.classNames
+                })}
+                role="row"
+                {...row?.props}
+              >
+                {data.map((d, i, a) => (
+                  <td
+                    key={createDataKey(i)}
+                    className={classNames(cellStyles, {
+                      [d.classNames!]: !!d.classNames,
+                      "rounded-bl-lg":
+                        i === 0 && rowIndex === rowArray.length - 1,
+                      "rounded-br-lg":
+                        i === a.length - 1 && rowIndex === rowArray.length - 1
+                    })}
+                    role="cell"
+                    {...d.props}
                   >
-                    {data.map((d, i) => (
-                      <td
-                        key={createDataKey(i)}
-                        className={`${cellStyles} ${d.classNames || ""}`}
-                        role="cell"
-                        {...d.props}
-                      >
-                        {d.title}
-                      </td>
-                    ))}
-                  </tr>
+                    {d.title}
+                  </td>
                 ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
