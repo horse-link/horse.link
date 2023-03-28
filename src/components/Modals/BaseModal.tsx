bd0ce6bfef41ef8e875af873c4f29ba0b307dd73
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-28 13:50:46 +1000

    fix: logic

--- src/components/Modals/BaseModal.tsx
+++ src/components/Modals/BaseModal.tsx
@@ -16,7 +16,7 @@ export const BaseModal: React.FC<Props> = ({
   isLarge
 }) => (
   <Transition appear show={isOpen} as={Fragment}>
-    <Dialog as="div" className="relative z-10" onClose={onClose}>
+    <Dialog as="div" className="relative z-50" onClose={onClose}>
       <Transition.Child
         as={Fragment}
         enter="ease-out duration-300"
