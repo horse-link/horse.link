b7270b19496dbff45668397506f471ce64aa3171
Barry Earsman <barry@earsman.com>
2023-03-29 15:39:08 +1000

    chore: remove unnecessary assignment, type http responses

--- src/apis/Api.ts
+++ src/apis/Api.ts
@@ -123,28 +123,31 @@ export class Api {
   };
 
   public getMarketAddresses = async (): Promise<string[]> => {
-    const { data } = await this.client.get("/markets/");
+    const { data } = await this.client.get<string[]>("/markets/");
     return data;
   };
 
   public getMarketDetail = async (marketAddress: string): Promise<Market> => {
-    const { data } = await this.client.get(`/markets/${marketAddress}`);
+    const { data } = await this.client.get<Market>(`/markets/${marketAddress}`);
     return data;
   };
 
   public getVaultAddresses = async (): Promise<string[]> => {
-    const { data } = await this.client.get("/vaults/");
+    const { data } = await this.client.get<string[]>("/vaults/");
     return data;
   };
 
   public getVaultDetail = async (vaultAddress: string): Promise<VaultInfo> => {
-    const { data } = await this.client.get(`/vaults/${vaultAddress}`);
-    data.address = vaultAddress;
+    const { data } = await this.client.get<VaultInfo>(
+      `/vaults/${vaultAddress}`
+    );
     return data as VaultInfo;
   };
 
   public getVaultToken = async (vaultAddress: string): Promise<Token> => {
-    const { data } = await this.client.get(`/vaults/${vaultAddress}/token`);
+    const { data } = await this.client.get<Token>(
+      `/vaults/${vaultAddress}/token`
+    );
     return data;
   };
 
