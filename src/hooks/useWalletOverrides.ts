ee39936b7fb3229a8bebb39b6a0c303870717be0
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-31 16:37:28 +1000

    chore: network intent

--- src/hooks/useWalletOverrides.ts
+++ src/hooks/useWalletOverrides.ts
@@ -1,7 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   Chain,
-  Connector,
   useAccount,
   useConnect,
   useNetwork,
@@ -10,65 +9,67 @@ import {
 import { LOCAL_WALLET_ID } from "../constants/wagmi";
 import { useApiWithForce } from "../providers/Api";
 import { useApolloWithForce } from "../providers/Apollo";
-import { Network } from "../types/general";
 import { useLocation } from "react-router-dom";
 import { arbitrum } from "@wagmi/chains";
+import constants from "../constants";
 
 // beware all ye whom enter
 // here lie the dreaded overrides
 // to slay the plethora of dragons wagmi spawns
 
 export const useWalletOverrides = () => {
-  const { chain, chains } = useNetwork();
+  const { chain } = useNetwork();
   const { connectors, connect } = useConnect();
   const { connector, isConnected } = useAccount();
-  const { switchNetwork } = useSwitchNetwork();
+  const { switchNetwork, isError } = useSwitchNetwork();
   const { forceNewChain: forceApi } = useApiWithForce();
   const { forceNewChain: forceApollo } = useApolloWithForce();
   const { pathname } = useLocation();
 
-  const [lastKnownChain, setLastKnownChain] = useState<Network>(
-    chain || chains[0]
-  );
-  const previousChain = useRef<Network>();
+  const isLocalWallet = connector?.id === LOCAL_WALLET_ID;
+  const isChainUnsupported = chain?.unsupported || false;
+  const isUnsupportedPage = pathname === "/unsupported";
+
   const [lastKnownConnector, setLastKnownConnector] = useState(
     connector || connectors[0]
   );
-  const previousConnector = useRef<Connector>();
-  const isLocalWallet = lastKnownConnector.id === LOCAL_WALLET_ID;
-  // undefined = loading, boolean = value
-  const isChainUnsupported = lastKnownChain?.unsupported || false;
+  useEffect(() => {
+    if (!connector) return;
 
-  // unsupported page check
-  const isUnsupportedPage = pathname === "/unsupported";
+    setLastKnownConnector(connector);
+  }, [connector]);
 
-  const forceNewNetwork = (chain: Chain) => {
-    switchNetwork?.(chain.id);
-    forceApi(chain);
-    forceApollo(chain);
-  };
+  // network intent
+  const networkIntent = useRef<Chain>(constants.blockchain.CHAINS[0]);
+
+  useLayoutEffect(() => {
+    if (!chain || isLocalWallet || chain.id !== networkIntent.current.id)
+      return;
+
+    networkIntent.current = chain;
+  }, [chain]);
 
   useEffect(() => {
     if (!chain) return;
 
-    setLastKnownChain(prev => {
-      previousChain.current = prev;
-      return chain;
-    });
-  }, [chain, lastKnownConnector]);
+    networkIntent.current = chain;
+  }, [isError]);
 
-  useEffect(() => {
-    if (!connector) return;
-
-    setLastKnownConnector(prev => {
-      previousConnector.current = prev;
-      return connector;
-    });
-  }, [connector]);
+  const forceNewNetwork = (chain: Chain) => {
+    try {
+      networkIntent.current = chain;
+      switchNetwork?.(chain.id);
+      forceApi(chain);
+      forceApollo(chain);
+    } catch (e) {
+      console.error(e);
+      networkIntent.current = constants.blockchain.CHAINS[0];
+    }
+  };
 
   // reconnect
   useEffect(() => {
-    if (isConnected || !lastKnownChain || !lastKnownConnector) return;
+    if (isConnected) return;
 
     // unsupported page override
     if (isUnsupportedPage) {
@@ -80,24 +81,18 @@ export const useWalletOverrides = () => {
     }
 
     connect({
-      chainId: lastKnownChain.id,
+      chainId: networkIntent.current.id,
       connector: lastKnownConnector
     });
   }, [isConnected]);
 
   useEffect(() => {
-    if (
-      lastKnownConnector.id === previousConnector.current?.id ||
-      !previousChain.current ||
-      isUnsupportedPage
-    )
-      return;
+    if (isUnsupportedPage) return;
 
-    forceNewNetwork(previousChain.current);
+    forceNewNetwork(networkIntent.current);
   }, [lastKnownConnector]);
 
   return {
-    lastKnownChain,
     lastKnownConnector,
     isLocalWallet,
     forceNewNetwork,
