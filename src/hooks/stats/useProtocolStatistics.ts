decc8215ba936e2ce2ac32ee4fa75878ec2c66fb
Barry Earsman <barry@earsman.com>
2023-04-12 17:06:57 +1000

    chore: remove redundant function

--- src/hooks/stats/useProtocolStatistics.ts
+++ src/hooks/stats/useProtocolStatistics.ts
@@ -1,38 +1,6 @@
-import { BigNumber } from "ethers";
-import { useEffect, useMemo, useState } from "react";
-import { FormattedProtocol, Protocol } from "../../types/subgraph";
-import useSubgraph from "../useSubgraph";
-import utils from "../../utils";
+import { useEffect, useState } from "react";
+import { FormattedProtocol } from "../../types/subgraph";
 import { useApi } from "../../providers/Api";
-
-type Response = {
-  protocol: Protocol;
-};
-
-export const useProtocolStatisticsFromGraph = ():
-  | FormattedProtocol
-  | undefined => {
-  // TODO: Consider using API query here -- subgraph is incorrect for Collateralised markets
-  const { data, loading } = useSubgraph<Response>(
-    utils.queries.getProtocolStatsQuery()
-  );
-
-  const formattedData = useMemo<FormattedProtocol | undefined>(() => {
-    if (loading || !data) return;
-
-    const protocol = data.protocol;
-
-    return {
-      id: protocol?.id ?? "none",
-      inPlay: protocol?.inPlay ?? 0,
-      tvl: protocol?.currentTvl ?? 0,
-      performance: +protocol?.performance ?? 100,
-      lastUpdate: +protocol?.lastUpdate ?? 0
-    };
-  }, [data, loading]);
-
-  return formattedData;
-};
 
 export const useProtocolStatistics = (): FormattedProtocol | undefined => {
   const api = useApi();
