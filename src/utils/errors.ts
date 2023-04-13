09afead9cc0150a47fbe9b3a2442b48177734903
Barry Earsman <barry@earsman.com>
2023-04-13 13:10:02 +1000

    feat: Handle blockchain errors with helpful messages

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,28 +1,92 @@
 import { ethers } from "ethers";
 
-export const getMeaningfulMessage = (error: any) => {
-  const stringified = JSON.stringify(error.message).toLowerCase();
-  switch (true) {
-    case stringified.includes("locked"):
-      return "Locked time not passed";
-    case stringified.includes("signature"):
-      return "Invalid signature";
-    case stringified.includes("date"):
-      return "Invalid time";
-    // include other cases that require a more descriptive error message here
-  }
+enum ContractError {
+  BackInvalidDate = "_back: Invalid date",
+  BackInvalidSignature = "back: Invalid signature",
+  BackOracleAlreadySet = "back: Oracle result already set for this market",
+  BackPayoutDateNotReached = "_payout: Payout date not reached",
+  BetAlreadySettled = "Bet has already settled",
+  OracleCheckResultNotSet = "checkResult: Invalid propositionId",
+  OracleGetResultNotSet = "getResult: Invalid propositionId",
+  OracleSetResultAlreadySet = "setResult: Result already set",
+  OracleSetResultInvalidPropositionId = "setResult: Invalid propositionId",
+  OracleSetResultInvalidSignature = "setResult: Invalid signature",
+  OracleSetScratchedResultAlreadySet = "setScratchedResult: Result already set",
+  OracleSetScratchedResultInvalidPropositionId = "setScratchedResult: Invalid propositionId",
+  OracleSetScratchedResultInvalidSignature = "setScratchedResult: Invalid signature",
+  VaultTransferLockedTimeNotPassed = "_transfer: Locked time not passed",
+  VaultWithdrawLockedTimeNotPassed = "_withdraw: Locked time not passed"
+}
 
-  const code = error.code as ethers.errors;
-  switch (code) {
-    case ethers.errors.ACTION_REJECTED:
-      return "Action rejected";
-    case ethers.errors.NETWORK_ERROR:
-      return "Network error";
-    case ethers.errors.INSUFFICIENT_FUNDS:
-      return "Insufficient funds";
-    case ethers.errors.UNPREDICTABLE_GAS_LIMIT:
-      return "Invalid transaction";
-    default:
-      return `Unknown error encountered: ${error}`;
+const contractErrorLookup: Record<ContractError, string> = {
+  [ContractError.BackInvalidDate]: "Betting has closed on this race.",
+  [ContractError.BackInvalidSignature]:
+    "We weren't able to verify your bet. Please refresh the page and try again. If the issue persists, please contact us.",
+  [ContractError.BackOracleAlreadySet]:
+    "Someone else has already registered the result for this race. Please try again.",
+  [ContractError.BetAlreadySettled]: "No worries! The bet has already settled.",
+  [ContractError.OracleCheckResultNotSet]:
+    "The result for this race has not been registered yet.",
+  [ContractError.OracleGetResultNotSet]:
+    "The result for this race has not been registered yet.",
+  [ContractError.OracleSetResultInvalidPropositionId]:
+    "Something went wrong while registering the result for this race. Please refresh the page and try again. If the issue persists, please contact us.",
+  [ContractError.OracleSetResultInvalidSignature]:
+    "We weren't able to verify your transaction to set the result for this race. Please refresh the page and try again. If the issue persists, please contact us.",
+  [ContractError.OracleSetScratchedResultAlreadySet]:
+    "Someone else already registered the scratched runner. Please try again.",
+  [ContractError.OracleSetScratchedResultInvalidPropositionId]:
+    "Something went wrong while registering a scratched runner. Please refresh the page and try again. If the issue persists, please contact us.",
+  [ContractError.OracleSetScratchedResultInvalidSignature]:
+    "We weren't able to verify your transaction to register a scratched runner. Please refresh the page and try again. If the issue persists, please contact us.",
+  [ContractError.VaultTransferLockedTimeNotPassed]:
+    "You can't redeem your vault shares yet. You must wait until the locked period has elapsed.",
+  [ContractError.VaultWithdrawLockedTimeNotPassed]:
+    "You can't trade your vault shares yet. You must wait until the locked period has elapsed."
+};
+
+const DEFAULT_BLOCKCHAIN_ERROR =
+  "Something went wrong while processing your transaction. Please refresh the page and try again. If the issue persists, please contact us.";
+const DEFAULT_ERROR_MESSAGE =
+  "Something went wrong. Please refresh the page and try again. If the issue persists, please contact us.";
+
+const ethersErrorLookup: Record<ethers.errors, string> = {
+  [ethers.errors.ACTION_REJECTED]:
+    "It looks like you rejected the transaction. Please try again.",
+  [ethers.errors.NETWORK_ERROR]:
+    "We're having trouble connecting to the network. Please check your internet connection and try again.",
+  [ethers.errors.INSUFFICIENT_FUNDS]:
+    "You don't have enough 'gas' to send this transaction. You can get some more through the faucet.",
+  [ethers.errors.UNPREDICTABLE_GAS_LIMIT]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.CALL_EXCEPTION]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.SERVER_ERROR]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.TIMEOUT]:
+    "We're having trouble connecting to the network. Please check your internet connection and try again.",
+  [ethers.errors.UNKNOWN_ERROR]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.NOT_IMPLEMENTED]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.UNSUPPORTED_OPERATION]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.BUFFER_OVERRUN]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.NUMERIC_FAULT]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.MISSING_NEW]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.INVALID_ARGUMENT]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.MISSING_ARGUMENT]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.UNEXPECTED_ARGUMENT]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.NONCE_EXPIRED]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.REPLACEMENT_UNDERPRICED]: DEFAULT_BLOCKCHAIN_ERROR,
+  [ethers.errors.TRANSACTION_REPLACED]: DEFAULT_BLOCKCHAIN_ERROR
+};
+
+export const getMeaningfulMessage = (error: any) => {
+  console.error(error);
+  const stringified = JSON.stringify(error.message).toLowerCase();
+  const contractError = Object.keys(contractErrorLookup).find(key =>
+    stringified.includes(key.toLowerCase())
+  );
+  if (contractError) {
+    return contractErrorLookup[contractError as ContractError];
   }
+  if (error?.code && ethersErrorLookup[error?.code as ethers.errors]) {
+    return ethersErrorLookup[error.code as ethers.errors];
+  }
+  return DEFAULT_ERROR_MESSAGE;
 };
