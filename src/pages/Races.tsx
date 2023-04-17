84ff6382f015ee0647dfdf24669724410fdeaf90
Barry Earsman <barry@earsman.com>
2023-04-17 10:16:50 +1000

    fix: compile issue preventing deployment

--- src/pages/Races.tsx
+++ src/pages/Races.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useRunnersData, useMeetData } from "../hooks/data";
 import { RacesButton } from "../components/Buttons";
