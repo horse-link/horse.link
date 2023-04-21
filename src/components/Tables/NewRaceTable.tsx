08f977c8e887c2e013f50668ad355dfd415a724f
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-04-21 14:41:15 +1000

    feat: scratched style

--- src/components/Tables/NewRaceTable.tsx
+++ src/components/Tables/NewRaceTable.tsx
@@ -7,7 +7,7 @@ import { ethers } from "ethers";
 import utils from "../../utils";
 
 type Props = {
-  runners?: Runner[];
+  runners?: Array<Runner>;
   totalBetsOnPropositions?: TotalBetsOnPropositions;
   setSelectedRunner: (runner?: Runner) => void;
   setIsModalOpen: (open: boolean) => void;
@@ -50,64 +50,74 @@ export const NewRaceTable: React.FC<Props> = ({
     </div>
   ));
 
+  const runnerMapping = (runner: Runner, i: number) => {
+    const stats = totalBetsOnPropositions?.[runner.proposition_id || ""];
+    const formattedBacked = stats
+      ? ethers.utils.formatEther((+stats.amount).toString())
+      : "0.0000";
+
+    const style = classNames("w-full text-left py-4", {
+      "line-through": runner.status != "Open"
+    });
+
+    return [
+      ...(
+        ["number", "name", "last5Starts", "handicapWeight", "odds"] as Array<
+          keyof typeof runner
+        >
+      ).map((key, i) => (
+        <div
+          className={classNames(style, {
+            "text-hl-secondary": [1, 4].includes(i)
+          })}
+          key={`runnertable-${runner.proposition_id}-${key.toString()}-${i}`}
+          onClick={() => onClickRunner(runner)}
+        >
+          {key === "odds"
+            ? utils.formatting.formatToTwoDecimals(runner[key].toString())
+            : runner[key].toString()}
+        </div>
+      )),
+      <div
+        className={style}
+        key={`runnertable-${runner.proposition_id}-${i}`}
+        onClick={() => onClickRunner(runner)}
+      >
+        {utils.formatting.formatToFourDecimals(formattedBacked)}
+      </div>,
+      <div
+        className={classNames(style, "text-hl-secondary")}
+        key={`runnertable-${runner.proposition_id}-${i}`}
+        onClick={() => onClickRunner(runner)}
+      >
+        {utils.formatting.formatToFourDecimals(
+          stats?.percentage.toString() || "0"
+        )}
+      </div>
+    ];
+  };
+
   const rows = runners
-    ? runners.map((runner, i) => {
-        const stats = totalBetsOnPropositions?.[runner.proposition_id || ""];
-        const formattedBacked = stats
-          ? ethers.utils.formatEther((+stats.amount).toString())
-          : "0.0000";
-
-        const style = "w-full text-left py-4";
-
-        return [
-          ...(
-            [
-              "number",
-              "name",
-              "last5Starts",
-              "handicapWeight",
-              "odds"
-            ] as Array<keyof typeof runner>
-          ).map((key, i) => (
-            <div
-              className={classNames(style, {
-                "text-hl-secondary": [1, 4].includes(i)
-              })}
-              key={`runnertable-${
-                runner.proposition_id
-              }-${key.toString()}-${i}`}
-              onClick={() => onClickRunner(runner)}
-            >
-              {key === "odds"
-                ? utils.formatting.formatToTwoDecimals(runner[key].toString())
-                : runner[key].toString()}
-            </div>
-          )),
-          <div
-            className={style}
-            key={`runnertable-${runner.proposition_id}-${i}`}
-            onClick={() => onClickRunner(runner)}
-          >
-            {utils.formatting.formatToFourDecimals(formattedBacked)}
-          </div>,
-          <div
-            className={classNames(style, "text-hl-secondary")}
-            key={`runnertable-${runner.proposition_id}-${i}`}
-            onClick={() => onClickRunner(runner)}
-          >
-            {utils.formatting.formatToFourDecimals(
-              stats?.percentage.toString() || "0"
-            )}
-          </div>
-        ];
-      })
+    ? [
+        ...runners.filter(r => r.status === "Open").map(runnerMapping),
+        ...runners.filter(r => r.status != "Open").map(runnerMapping)
+      ]
     : [];
+
+  const loading = [
+    [
+      <div key="racetable-loading-blank" />,
+      <div className="py-4" key="racetable-loading-message">
+        Loading...
+      </div>
+    ]
+  ];
 
   return (
     <NewTable
       headers={headers}
       headerStyles="font-basement tracking-wider"
-      rows={rows}
+      rows={!runners?.length ? loading : rows}
       rowStyles={classNames({
         "hover:bg-hl-primary cursor-pointer hover:!text-hl-secondary": !closed
       })}
