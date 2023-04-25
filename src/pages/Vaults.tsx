44d1954eb212bd4e940e64d9dab5ae83b4cea3b1
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-04-25 21:52:14 +1000

    feat: mobile vaults page

--- src/pages/Vaults.tsx
+++ src/pages/Vaults.tsx
@@ -23,7 +23,7 @@ const Vaults: React.FC = () => {
 
   return (
     <PageLayout>
-      <div className="grid grid-cols-2 gap-4 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-4">
         <Card
           title="24H Vault Volume"
           data={
