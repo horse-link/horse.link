133958f63354d89c9ca3485e5693a9d4bf6466b6
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-04-26 13:04:05 +1000

    fix: subgraph url

--- src/providers/Apollo.tsx
+++ src/providers/Apollo.tsx
@@ -41,7 +41,7 @@ export const ApolloProvider: React.FC<{ children: React.ReactNode }> = ({
     () => ({
       client: new ApolloClient({
         link: new HttpLink({
-          uri: `${constants.env.SUBGRAPH_URL}-dev-${suffix}`,
+          uri: `${constants.env.SUBGRAPH_URL}-${suffix}`,
           fetch
         }),
         cache: new InMemoryCache()
