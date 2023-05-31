4a487adea6b816407a3ae91f814dd28160b50714
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-05-31 13:20:34 +1000

    chore: remove calculateRaceMargin

--- src/utils/races.ts
+++ src/utils/races.ts
@@ -1,35 +1,10 @@
 import { Race, Runner, Meet } from "../types/meets";
-import { ethers } from "ethers";
-import constants from "../constants";
 import { RaceStatus } from "../constants/status";
 import utils from "../utils";
 import dayjs, { Dayjs } from "dayjs";
 
 export const isScratchedRunner = (runner: Runner) =>
   ["LateScratched", "Scratched"].includes(runner.status);
-
-// REMOVE THIS FROM THE FRONTEND
-export const calculateRaceMargin = (odds: number[]) => {
-  // all ones case
-  if (odds.every(odd => odd == 1)) {
-    return "1";
-  }
-
-  // if odds arent present, use 1 odds (prevents division by zero error)
-  const filteredOdds = odds.map(o => o || 1);
-
-  const bnOdds = filteredOdds.map(o => ethers.utils.parseEther(o.toString()));
-  const oneOverOdds = bnOdds.map(o =>
-    constants.blockchain.ONE_ETHER.mul(ethers.constants.WeiPerEther).div(o)
-  );
-
-  const summed = oneOverOdds.reduce(
-    (sum, cur) => sum.add(cur),
-    ethers.constants.Zero
-  );
-
-  return ethers.utils.formatEther(summed);
-};
 
 export const createRacingLink = (race: Race, meet: Meet) =>
   race.status !== RaceStatus.PAYING
