a666cb45fd0fd1ebd33cb40a8266f64f6a6182b8
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-05-31 15:14:50 +1000

    fix: bets

--- src/hooks/data/useBetsData.ts
+++ src/hooks/data/useBetsData.ts
@@ -2,7 +2,9 @@ import useSwr from "../useSwr";
 import { BetHistoryResponse2 } from "../../types/bets";
 
 export const useBetsData = () => {
-  const { data } = useSwr<Array<BetHistoryResponse2>>(`/bets/history`);
+  const { data } = useSwr<{
+    bets: Array<BetHistoryResponse2>;
+  }>(`/bets/history`);
 
-  return data;
+  return data?.bets;
 };
