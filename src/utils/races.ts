e31596a59b20e5f469d6f454493d32e1192c7d5c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-06-12 08:40:00 +1000

    chore: remove margin from FE

--- src/utils/races.ts
+++ src/utils/races.ts
@@ -2,8 +2,6 @@ import { Race, Runner, Meet } from "../types/meets";
 import { RaceStatus } from "../constants/status";
 import utils from "../utils";
 import dayjs, { Dayjs } from "dayjs";
-import { ethers } from "ethers";
-import constants from "../constants";
 
 export const isScratchedRunner = (runner: Runner) =>
   ["LateScratched", "Scratched"].includes(runner.status);
@@ -28,26 +26,4 @@ export const createCellText = (race: Race, now: Dayjs) => {
     : isAfterClosingTime
     ? "CLSD"
     : timeString;
-};
-
-export const calculateRaceMargin = (odds: number[]) => {
-  // all ones case
-  if (odds.every(odd => odd == 1)) {
-    return "1";
-  }
-
-  // if odds arent present, use 1 odds (prevents division by zero error)
-  const filteredOdds = odds.map(o => o || 1);
-
-  const bnOdds = filteredOdds.map(o => ethers.utils.parseEther(o.toString()));
-  const oneOverOdds = bnOdds.map(o =>
-    constants.blockchain.ONE_ETHER.mul(ethers.constants.WeiPerEther).div(o)
-  );
-
-  const summed = oneOverOdds.reduce(
-    (sum, cur) => sum.add(cur),
-    ethers.constants.Zero
-  );
-
-  return ethers.utils.formatEther(summed);
 };
